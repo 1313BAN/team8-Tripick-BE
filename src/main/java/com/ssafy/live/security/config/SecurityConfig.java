5a9149028a0440d2b2d65015a73b0f6545ee20ed
noah <flhbigfamily7@gmail.com>
2025-05-13 12:01:57 +0900

    fix add age field to users table

--- src/main/java/com/ssafy/live/security/config/SecurityConfig.java
+++ src/main/java/com/ssafy/live/security/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
     		    .csrf(csrf -> csrf.disable())
     		    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
     		    .authorizeHttpRequests(auth -> auth
-    		        //.requestMatchers("/api/users/login", "/api/users/signup").permitAll()
-    		        .anyRequest().permitAll()
+    		        .requestMatchers("/api/users/login", "/api/users/signup").permitAll()
+    		        .anyRequest().authenticated()
     		    )
     		    .exceptionHandling(ex -> ex
     		        .authenticationEntryPoint(entryPoint)
