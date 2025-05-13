5af20847252dcabb8a4099f67c7d6e95baef4b4d
noah <flhbigfamily7@gmail.com>
2025-05-13 12:07:50 +0900

    chore permit all to any request for test

--- src/main/java/com/ssafy/live/security/config/SecurityConfig.java
+++ src/main/java/com/ssafy/live/security/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
     		    .csrf(csrf -> csrf.disable())
     		    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
     		    .authorizeHttpRequests(auth -> auth
-    		        .requestMatchers("/api/users/login", "/api/users/signup").permitAll()
-    		        .anyRequest().authenticated()
+    		        //.requestMatchers("/api/users/login", "/api/users/signup").permitAll()
+    		        .anyRequest().permitAll()
     		    )
     		    .exceptionHandling(ex -> ex
     		        .authenticationEntryPoint(entryPoint)
