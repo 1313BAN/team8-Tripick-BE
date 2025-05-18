617363286a4c919836abd0796708b1151aefada7
NOAH <flhbigfamily7@gmail.com>
2025-05-19 01:48:25 +0900

    [feat] add user test code

--- src/main/java/com/ssafy/live/config/WebConfig.java
+++ src/main/java/com/ssafy/live/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // 또는 "/**"
-                        .allowedOrigins("http://127.0.0.1:5500") // 일단 임시로 포
+                        .allowedOrigins(
+                                "http://127.0.0.1:5500",
+                                "http://localhost:5173",
+                                "http://127.0.0.1:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(false);
