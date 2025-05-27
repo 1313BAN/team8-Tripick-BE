1b329df492753490df2c8b6d0446ac0be15811bc
NOAH <flhbigfamily7@gmail.com>
2025-05-27 10:18:23 +0900

    [fix] boot앱이 켜지면 redis에 자동삽입하는 코드 주석처리

--- backend/src/main/java/com/ssafy/live/global/init/SpotVectorInitializer.java
+++ backend/src/main/java/com/ssafy/live/global/init/SpotVectorInitializer.java
@@ -1,35 +1,35 @@
-package com.ssafy.live.global.init;
+// package com.ssafy.live.global.init;
 
-import com.ssafy.live.domain.spot.service.SpotVectorService;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
+// import com.ssafy.live.domain.spot.service.SpotVectorService;
+// import jakarta.annotation.PostConstruct;
+// import lombok.RequiredArgsConstructor;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Component;
+// import org.springframework.context.annotation.Profile;
+// import org.springframework.data.redis.core.StringRedisTemplate;
+// import org.springframework.stereotype.Component;
 
-@Component
-@RequiredArgsConstructor
-@Profile("!test")
-public class SpotVectorInitializer {
+// @Component
+// @RequiredArgsConstructor
+// @Profile("!test")
+// public class SpotVectorInitializer {
 
-    private final SpotVectorService spotVectorService;
-    private final StringRedisTemplate redisTemplate;
+//     private final SpotVectorService spotVectorService;
+//     private final StringRedisTemplate redisTemplate;
 
-    private static final String INIT_KEY = "vector:init";
+//     private static final String INIT_KEY = "vector:init";
 
-    @PostConstruct
-    public void init() {
-        Boolean alreadyInitialized = redisTemplate.hasKey(INIT_KEY);
+//     @PostConstruct
+//     public void init() {
+//         Boolean alreadyInitialized = redisTemplate.hasKey(INIT_KEY);
 
-        if (Boolean.FALSE.equals(alreadyInitialized)) {
-            System.out.println("📌 Redis에 초기 데이터 삽입 시작...");
-            spotVectorService.storeAllToVector();
+//         if (Boolean.FALSE.equals(alreadyInitialized)) {
+//             System.out.println("📌 Redis에 초기 데이터 삽입 시작...");
+//             spotVectorService.storeAllToVector();
 
-            // 삽입 완료 표시
-            redisTemplate.opsForValue().set(INIT_KEY, "true");
-        } else {
-            System.out.println("✅ 이미 초기화 완료됨. 삽입 생략.");
-        }
-    }
-}
+//             // 삽입 완료 표시
+//             redisTemplate.opsForValue().set(INIT_KEY, "true");
+//         } else {
+//             System.out.println("✅ 이미 초기화 완료됨. 삽입 생략.");
+//         }
+//     }
+// }
